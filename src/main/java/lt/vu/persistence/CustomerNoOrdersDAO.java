af2840e3d7ba8c726feca22ab10a0f09aab07e84
Vilius Macijauskas <macijauskas.vilius@gmail.com>
2022-05-25 15:30:16 +0300

    PSK2 - 3

--- src/main/java/lt/vu/persistence/CustomerNoOrdersDAO.java
+++ src/main/java/lt/vu/persistence/CustomerNoOrdersDAO.java
@@ -3,15 +3,18 @@ package lt.vu.persistence;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
 
 import lt.vu.entities.Customer;
 
 @Alternative
-public class CustomerNoOrdersDAO extends CustomerDAO {
+@ApplicationScoped
+public class CustomerNoOrdersDAO extends CustomerDAO implements ICustomerDAO {
 
     @Override
     public List<Customer> loadAll() {
+        System.out.println("Alternative was CALLED.");
         List<Customer> customersWithNoOrders = new ArrayList<>();
         for (Customer customer: super.loadAll()) {
             if (customer.getOrders().isEmpty()) {
